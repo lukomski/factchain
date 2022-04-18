7881a7e9687347f5e58312857d563874e22e4329
lukomski <jlukomski1@gmail.com>
2022-04-18 22:54:02 +0200

    use dependency injection

--- Program.cs
+++ Program.cs
@@ -1,27 +1,46 @@
 ﻿using Newtonsoft.Json.Linq; 
 
-class MainClass
-{
-    static readonly HttpClient client = new HttpClient();
 
-    public static async Task FetchNewFact(string filename)
+internal class WorkerClass {
+
+    private readonly HttpClient _client;
+    private readonly string _filename;
+    public WorkerClass(HttpClient client, string filename) {
+        _client = client;
+        _filename = filename;
+    }
+
+    private async Task<string> GetFact()
     {
-        // make fetch
-        HttpResponseMessage response = await client.GetAsync("https://catfact.ninja/fact/");
+        HttpResponseMessage response = await _client.GetAsync("https://catfact.ninja/fact/");
         response.EnsureSuccessStatusCode();
-        var responseBody = await response.Content.ReadAsStringAsync();
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    private async Task Save(string filename, string message)
+    {
+        using StreamWriter file = new(filename, append: true);
+        await file.WriteLineAsync(message);
+    }
+
+    public void FetchNewFact()
+    {
+        var responseBody = GetFact().GetAwaiter().GetResult();
 
         // parse output
         dynamic jToken = JToken.Parse(responseBody);
         string message = jToken.fact;
 
-        // append file
-        using StreamWriter file = new(filename, append: true);
-        await file.WriteLineAsync(message);
+        Save(_filename, message).GetAwaiter().GetResult();
     }
+}
 
+public class MainClass
+{
+    static readonly HttpClient client = new HttpClient();
     static void Main() {
         string fileName = "out.txt";
-        FetchNewFact(fileName).GetAwaiter().GetResult();
+        WorkerClass workerClass = new WorkerClass(client, fileName);    
+        workerClass.FetchNewFact();
     }
 }
